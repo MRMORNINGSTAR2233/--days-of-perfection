112882e74c74cf52b95e2ebe5a47af506915041b
MRMORNINGSTAR2233 <aksh9881@gmail.com>
2024-06-13 20:21:10 +0530

    Day two of 100 learnt Rust basics and did Leet

---
+++ RUST/practise/target/debug/deps/practise-3b8ecaf9265d42bc.d
@@ -0,0 +1,5 @@
+/Users/akshay/Documents/--days-of-perfection/RUST/practise/target/debug/deps/practise-3b8ecaf9265d42bc: src/main.rs
+
+/Users/akshay/Documents/--days-of-perfection/RUST/practise/target/debug/deps/practise-3b8ecaf9265d42bc.d: src/main.rs
+
+src/main.rs:
---
+++ RUST/practise/target/debug/practise.d
@@ -0,0 +1,1 @@
+/Users/akshay/Documents/--days-of-perfection/RUST/practise/target/debug/practise: /Users/akshay/Documents/--days-of-perfection/RUST/practise/src/main.rs
